5de4043776e089931a333ed6d03ad7332e6335ea
amourey <amourey@users.noreply.github.com>
2017-06-12 16:57:16 -0400

    Setting variables in script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+var name = 'John';
+
+var lastName = 'Smith';
+console.log(name + " " + lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
