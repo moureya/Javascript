6fb99db617f7b32a09e10fd66ba8f57df37e296b
amourey <amourey@users.noreply.github.com>
2017-06-14 13:06:43 -0400

    Variable Mutation and Type Coercion

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Lecture: variables
+/*
 var name = 'John';
 
 var lastName = 'Smith';
@@ -8,3 +10,34 @@ console.log(age);
 
 var fullAge = true;
 console.log(fullAge);
+*/
+
+
+// Lecture: Variable Mutation and Type Coercion
+//var name = "John";
+var age = 26;
+
+//console.log(name +" "+ age);
+//console.log(age + age);
+
+
+var job, isMarried;
+
+job = "teacher"
+isMarried = true;
+
+console.log(name + " is a " + age + " year old "
++ job + ". Is he married? " + isMarried + ".");
+
+age = "thirty six";
+job = "driver";
+
+console.log(name + " is a " + age + " year old "
++ job + ". Is he married? " + isMarried + ".");
+
+var lastName = prompt("What is your last name?")
+
+console.log(lastName);
+
+alert(name + " is a " + age + " year old "
++ job + ". Is he married? " + isMarried + ".");
