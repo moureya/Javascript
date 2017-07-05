a805882fd23d03eed0b4f2e4ad7546c70fdcdc22
amourey <amourey@users.noreply.github.com>
2017-07-05 16:00:27 -0400

    Objects and methods

--- script.js
+++ script.js
@@ -260,3 +260,47 @@ jane['isMarried'] = true;
 
 console.log(jane);
 */
+///////////////////////////////////////////////////////////////////
+// Lecture: Objects and methods
+
+// v1.0
+/*
+var john = {
+  name: 'John',
+  lastName: 'Smith',
+  yearOfBirth: 1990,
+  job: 'teacher',
+  isMarried: false,
+  family: ['Jane', 'Mark', 'Bob'],
+  calculateAge: function() {
+    return 2017 - this.yearOfBirth;
+  }
+};
+
+
+//console.log(john.calculateAge(1970));
+console.log(john.calculateAge());
+
+var age = john.calculateAge();
+john.age = age;
+
+console.log(john.age);
+*/
+
+// v2.0
+var john = {
+  name: 'John',
+  lastName: 'Smith',
+  yearOfBirth: 1990,
+  job: 'teacher',
+  isMarried: false,
+  family: ['Jane', 'Mark', 'Bob'],
+  calculateAge: function() {
+    //return 2017 - this.yearOfBirth;
+    //this refers to the object. In this case john.
+    this.age = 2017 - this.yearOfBirth;
+  }
+};
+
+john.calculateAge();
+console.log(john);
