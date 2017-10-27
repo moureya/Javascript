24e3250b8bf86bde604e140a9c40c451d3786986
Anthony Mourey <moureya01@gmail.com>
2017-10-27 19:48:39 -0400

    Section 5 Lecture 59

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -183,7 +183,7 @@ function game() {
 
 game();
 */
-
+/*
 (function () {
   var score = Math.random() * 10;
   console.log(score >= 5);
@@ -196,3 +196,42 @@ game();
   var score = Math.random() * 10;
   console.log(score >= 5 - goodLuck);
 })(5);
+*/
+
+
+//////////////////////////////////////////
+// Lecture: Closures
+
+function retirement(retirementAge) {
+  var a = ' years left until retirement.';
+  return function(yearOfBirth) {
+    var age = 2017 - yearOfBirth;
+    console.log((retirementAge - age) + a);
+  }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1989);
+retirementGermany(1989);
+retirementIceland(1989);
+
+
+//retirement(66)(1989);
+
+
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === 'designer') {
+        console.log(name + ', can you please explain what UX design is?');
+    } else if (job === 'teacher') {
+        console.log('What subject do you teach, ' + name + '?');
+    } else {
+        console.log('Hello ' + name + ', what do you do?');
+      }
+    }
+  }
+
+interviewQuestion('teacher')('John');
